78d82014723605715f0286c996e65dd334e76c00
niklas <23435250+Biospheere@users.noreply.github.com>
2019-04-06 19:44:34 +0200

    Added Linux support

--- src/main/java/de/scarpex/mrf/command/general/ClearCommand.java
+++ src/main/java/de/scarpex/mrf/command/general/ClearCommand.java
@@ -1,5 +1,7 @@
 package de.scarpex.mrf.command.general;
 
+import de.scarpex.mrf.chromedriver.ChromeDriverUtils;
+import de.scarpex.mrf.chromedriver.OSType;
 import de.scarpex.mrf.command.Command;
 
 import java.io.IOException;
@@ -15,7 +17,12 @@ public class ClearCommand extends Command {
     @Override
     public void execute(String[] args) {
         try {
-            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            if(ChromeDriverUtils.getOS() == OSType.WINDOWS){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
         } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
